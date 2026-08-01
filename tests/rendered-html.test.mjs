da837bacc9aa089f285a2c04506519ebb1ea72b8
Ahmet Karadaş <ahmett.karadas@icloud.com>
2026-08-01 20:08:34 +0300

    allow localized hero line breaks in tests

--- tests/rendered-html.test.mjs
+++ tests/rendered-html.test.mjs
@@ -47,7 +47,7 @@ test("renders localized Turkish and English experiences", async () => {
   assert.match(trHtml, /<html[^>]+lang=["']tr["']/i);
   assert.match(enHtml, /<html[^>]+lang=["']en["']/i);
   assert.match(trHtml, /Fikri, iz bırakan\s+filme dönüştürüyoruz\./);
-  assert.match(enHtml, /We turn ideas into films that leave a mark\./);
+  assert.match(enHtml, /We turn ideas into films\s+that leave a mark\./);
   assert.match(enHtml, /\/en/);
   assert.match(trHtml, /Sıfır Atık Haftası/);
   assert.match(trHtml, /VEX Robotics Türkiye/);
